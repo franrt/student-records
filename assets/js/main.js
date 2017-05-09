9ef68eb7fb65a510135c5c6ca38a75a64239855a
berenisser <berenisser@gmail.com>
2017-05-09 18:57:30 -0300

    div agregado, y primera funcion en proceso

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 
+//traemos el espacio div desde html con 
+var espacio = document.getElementById('espacio');
+var students = [];
+
 // Constructor para generar una nueva estudiante
 function Student(name, techSkills, lifeSkills) {
     this.name = name;
@@ -7,5 +11,13 @@ function Student(name, techSkills, lifeSkills) {
     this.status = "Active";
 };
 
-var students = [];
+
+//Método para imprimir un producto en html
+Producto.prototype.toHTML = function () {
+  var html = '';
+  html += //aqui tengo que usar el constructor para crear el formato e como aparecera cada vez que se agregue una alumna
+  return html;
+}
+
+
 
