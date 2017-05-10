3947c015e202092bd61c8722314926c2465ee9e6
berenisser <berenisser@gmail.com>
2017-05-09 21:05:11 -0300

    funcionalidades listas, solo falta hacer el for en programacion funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,20 +14,20 @@ function Student(name, techSkills, lifeSkills) {
 //Método para imprimir una estudiante en html
 Student.prototype.toHTML = function(){
   var html = '';
-  html += this.name.toUpperCase() + "<br>" + "Tech Skills " + this.techSkills +"<br>"+ "Life Skills " +this.lifeSkills;
+  html += this.name.toUpperCase() + "<br>" + "Tech Skills " + this.techSkills +"<br>"+ "Life Skills " +this.lifeSkills+ "<br><br>";
   return html;
 }
 
 //Función que une todas las personas guardadas en el array students
-function mergeHTML (){
+function mergeHTML(){
   var html = '';
   for (i=0; i<students.length; i++){
-    html += name[i].toHTML();
+    html += students[i].toHTML();
   }
   return html;
 }
 
-//función que imprime el ultimo estudiante luego de ingresarlo
+//Función que imprime el ultimo estudiante luego de ingresarlo
 function printHTML(html){
   espacio.innerHTML = '';
   espacio.innerHTML = html;
